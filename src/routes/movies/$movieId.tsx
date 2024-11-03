36d4b34825312a5a29eb1196fc71ae5319920022
Olivier Thierry <olivier.thierry@gmail.com>
2024-11-03 18:22:24 +0100

    Add pending component

--- src/routes/movies/$movieId.tsx
+++ src/routes/movies/$movieId.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { MovieDetailsPage } from "../../pages/MovieDetailsPage";
 import { fetchOneMovie } from "../../services";
+import { Loader } from "../../components";
 
 export const Route = createFileRoute("/movies/$movieId")({
   component: MovieDetailsPage,
   loader: async ({ params: { movieId } }) => fetchOneMovie(movieId),
+  pendingComponent: Loader,
 });
--- src/routes/movies/index.tsx
+++ src/routes/movies/index.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { MoviesListPage } from "../../pages/MoviesListPage";
 import { fetchMovies } from "../../services";
+import { Loader } from "../../components";
 
 export const Route = createFileRoute("/movies/")({
   component: MoviesListPage,
   loader: fetchMovies,
+  pendingComponent: Loader,
 });
