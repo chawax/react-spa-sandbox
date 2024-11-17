4fbe45c0288d4cc9458e5e6552e23fef47f754c0
Olivier Thierry <olivier.thierry@gmail.com>
2024-11-17 16:28:31 +0100

    Refactor movie details route

---
+++ src/components/ActorsList.tsx
@@ -0,0 +1,21 @@
+import { Actor } from "@/types";
+import ActorCard from "./ActorCard";
+
+type ActorsListProps = {
+  actors: Actor[];
+};
+
+export default function ActorsList({ actors }: ActorsListProps) {
+  return (
+    <section>
+      <h3 className="text-xl font-bold">Casting</h3>
+      <div className="flex flex-row flex-wrap sm:flex-nowrap sm:w-200">
+        {actors.map((actor) => {
+          return (
+            <ActorCard key={actor.id} className="p-1 basis-1/2" actor={actor} />
+          );
+        })}
+      </div>
+    </section>
+  );
+}
--- src/routes/movies/$movieId.tsx
+++ src/routes/movies/$movieId.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Loader } from "@/components";
+import { format, parse } from "date-fns";
+
+import { FavoriteButton, Loader } from "@/components";
+import ActorsList from "@/components/ActorsList";
 import { ErrorPage } from "@/pages/ErrorPage";
-import { MovieDetailsPage } from "@/pages/MovieDetailsPage";
 import { fetchOneMovie } from "@/services";
 
 export const Route = createFileRoute("/movies/$movieId")({
@@ -10,3 +12,58 @@ export const Route = createFileRoute("/movies/$movieId")({
   pendingComponent: Loader,
   errorComponent: ErrorPage,
 });
+
+const voteFormatter = new Intl.NumberFormat("fr-FR", {
+  style: "decimal",
+  maximumFractionDigits: 1,
+});
+
+function MovieDetailsPage() {
+  const movie = Route.useLoaderData();
+
+  const imgUrl = `https://image.tmdb.org/t/p/w500${movie.posterPath}`;
+  const formattedReleaseDate = format(
+    parse(movie.releaseDate, "yyyy-MM-dd", new Date()),
+    "dd/MM/yyyy",
+  );
+  const formattedVote = voteFormatter.format(movie.voteAverage);
+
+  return (
+    <article className="flex flex-col gap-4">
+      <h2 className="text-3xl font-bold">{movie.title}</h2>
+      <p className="text-md italic">{movie.tagline}</p>
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <img
+          src={imgUrl}
+          alt={movie.title}
+          width={500}
+          className="rounded w-fit sm:basis-1/4 sm:w-[100px]"
+        />
+
+        <div className="flex flex-col gap-4 sm:basis-3/4 sm:ml-4">
+          <FavoriteButton id={movie.id} />
+          <p className="text-md">{movie.overview}</p>
+          {movie.genres ? (
+            <ul className="list-none">
+              {movie.genres.map((genre) => (
+                <span
+                  key={genre}
+                  className="text-sm text-white text-center bg-stone-500 py-2 px-4 rounded-xl mr-1 mb-1 inline-block"
+                >
+                  {genre}
+                </span>
+              ))}
+            </ul>
+          ) : null}
+          <p className="text-sm">
+            Sortie le <span className="font-bold">{formattedReleaseDate}</span>
+          </p>
+          <p className="text-sm">
+            Note : <span className="font-bold">{formattedVote}</span>
+          </p>
+          {movie.actors ? <ActorsList actors={movie.actors} /> : null}
+        </div>
+      </div>
+    </article>
+  );
+}
